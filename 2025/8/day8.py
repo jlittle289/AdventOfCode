c82d5ce57b7f0c3869d4450169c2004040585f65
Zapa289 <jack.little212@gmail.com>
2025-12-08 12:15:40 -0600

    Day 8

---
+++ 2025/8/day8.py
@@ -0,0 +1,67 @@
+#!/bin/python3
+import time
+import math
+import networkx as nx
+
+def euc_distance(one, two) -> float:
+    x1,y1,z1 = one
+    x2,y2,z2 = two
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
+
+def part1(points:list[tuple], num_con):
+
+    points_list = points.copy()
+
+    distances = []
+    for _ in range(len(points_list)-1):
+        point = points_list.pop()
+        distances.extend([(point, sub_point) for sub_point in points_list])
+
+    distances.sort(key=lambda x:euc_distance(x[0], x[1]), reverse=True)
+
+    G = nx.Graph()
+    for u,v in distances[-num_con:]:
+        G.add_edge(u,v)
+
+    return math.prod(sorted(map(len,nx.connected_components(G)))[-3:])
+
+def part2(points:list[tuple], num_con):
+
+    G = nx.Graph()
+    for u in points:
+        G.add_node(u)
+
+    points_list = points.copy()
+
+    distances = []
+    for _ in range(len(points_list)-1):
+        point = points_list.pop()
+        distances.extend([(point, sub_point) for sub_point in points_list])
+
+    distances.sort(key=lambda x:euc_distance(x[0], x[1]))
+
+    for u,v in distances[:num_con]:
+        G.add_edge(u,v)
+
+    for u,v in distances[num_con:]:
+        G.add_edge(u,v)
+        if len(list(nx.connected_components(G))) == 1:
+            return u[0] * v[0]
+
+    return 0
+
+def process_input(data:str) ->list[tuple]:
+    return [tuple(map(int, row.split(","))) for row in data.strip().splitlines()]
+
+if __name__ == "__main__":
+    with open("input.txt") as file:
+        DATA = file.read().strip()
+
+    INPUT_DATA = process_input(DATA)
+    CONNECTIONS = 1000
+
+    start_time = time.time()
+    print(f"Part 1: {part1(INPUT_DATA, CONNECTIONS)} - {time.time() - start_time:.6f}s\n")
+
+    start_time = time.time()
+    print(f"Part 2: {part2(INPUT_DATA, CONNECTIONS)} - {time.time() - start_time:.6f}s")
